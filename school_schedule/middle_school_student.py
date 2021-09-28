5c7f8b635122012598815c3b1d2c04c1b8641be4
Shay <shay.phillips11@gmail.com>
2021-09-28 14:35:37 -0400

    middle school student class added

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,17 @@
 from .student import Student
 
 # add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation=False):
+        super().__init__(name, grade, classes) 
+        self.gets_transportation = gets_transportation
+    
+    def summary(self):
+        student_sumamry = super().summary()
+        transportation_message = self.display_transportation_message()
+
+        return "\n".join(student_sumamry, transportation_message)
+
+    def display_transportation_message(self):
+        gets_message = "gets" if self.gets_transportation else "doesn't get"
+        return f"{self.name} {gets_message} transportation"
